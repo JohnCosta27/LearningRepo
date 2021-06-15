f83b921dcddc3188fe76457307f3c23abfaf63f0
John Costa <johncosta027@gmail.com>
2021-06-16 00:16:12 +0100

    First commit

---
+++ Auth0/index.js
@@ -0,0 +1,45 @@
+const { auth, requiresAuth } = require('express-openid-connect');
+const express = require('express');
+const { createClient } = require('@supabase/supabase-js');
+require('dotenv').config();
+
+const app = express();
+const supabase = createClient(
+	'https://wkkvrbxyvkqbogjcwsxr.supabase.co',
+	process.env.SUPABASE_KEY
+);
+
+app.use(
+	auth({
+		authRequired: false,
+		auth0Logout: true,
+		issuerBaseURL: process.env.ISSUER_BASE_URL,
+		baseURL: process.env.BASE_URL,
+		clientID: process.env.CLIENT_ID,
+		secret: process.env.SECRET,
+	})
+);
+
+app.get('/', async (req, res) => {
+	if (req.oidc.isAuthenticated()) {
+		const { data, error } = await supabase
+			.from('users')
+			.insert([{ user_id: req.oidc.user.sub }]);
+
+		console.log(data);
+		console.log(error);
+
+		res.redirect('/profile');
+	} else {
+		res.send('Login to access app');
+	}
+});
+
+app.get('/profile', requiresAuth(), (req, res) => {
+	res.send(JSON.stringify(req.oidc.user));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+	console.log(`Listening on port ${port}`);
+});
---
+++ README.md
@@ -0,0 +1,8 @@
+# Learning Repo
+### A repository dedicated to me, learning all sorts of technologies. And maybe implementing them in the future in other projects.
+
+#### Stuff learnt/learning
+* Auth0/Supabase
+	* Auth0 Authentication
+	* Supabase PostgreSQL database
+	This little project involved Auth0 authentication using an express package _express-openid-connect_ which allows for user authentication in a Node.js/Express.js back-end. Supabase provides a nice PostgreSQL which _can_ scale, plus I know SQL but I never approached it at a high level.
