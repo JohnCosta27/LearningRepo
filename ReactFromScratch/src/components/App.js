a024e893573b0275fb2a99fa8f1d00339c9668b6
John Costa <johncosta027@gmail.com>
2021-06-17 21:53:26 +0100

    React App from scratch (Babel + Webpack)

---
+++ ReactFromScratch/src/components/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from '../index.css';
+
+const App = () => {
+    return (
+        <div>
+            <h1>React from Scratch!</h1>
+        </div>
+    );
+}
+
+export default App;
