db3e008bf6a50c6d345d086cc9e40053b05990fe
John Costa <johncosta027@gmail.com>
2021-08-10 12:08:49 +0100

    Started learning Tailwind, and have set commands for production CSS

---
+++ TailwindCSS/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+	purge: ['./dist/**/*.html', './dist/**/*.js'],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {},
+		container: {
+			center: true,
+			padding: '2rem',
+		},
+	},
+	variants: {
+		extend: {},
+	},
+	plugins: [],
+};
